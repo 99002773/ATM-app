d85d6802c0ac96fbdee7ea427f874e30b9be07a7
99002773 <72495902+99002773@users.noreply.github.com>
2020-10-09 13:26:58 +0530

    Update test_function.c

--- test/test_function.c
+++ test/test_function.c
@@ -51,10 +51,10 @@ void test_Deposit(void) {
 
 void test_Withdraw(void) {
   CU_ASSERT(14800 == Withdraw(700));
-  CU_ASSERT(2700 == Withdraw(300));
-  CU_ASSERT(3000 == Withdraw(200));
-  CU_ASSERT(6000 == Withdraw(100));
-   CU_ASSERT(14400 == Withdraw(600));
+  CU_ASSERT(14500 == Withdraw(300));
+  CU_ASSERT(14300 == Withdraw(200));
+  CU_ASSERT(14200 == Withdraw(100));
+   CU_ASSERT(13600 == Withdraw(600));
 
   /* Dummy fail*/
   CU_ASSERT(0 == Withdraw(6));
